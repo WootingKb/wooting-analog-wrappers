880d32a44a1d60fa9ec0109773ae957eac858841
Simon <simon.whyte.lb@gmail.com>
2019-08-16 11:36:30 +0100

    Updated to latest sdk submodule

--- WootingAnalogSDK.NET/WootingAnalogSDK.cs
+++ WootingAnalogSDK.NET/WootingAnalogSDK.cs
@@ -13,7 +13,7 @@ namespace WootingAnalogSDK.NET {
         VirtualKeyTranslate
     }    
     
-    public enum AnalogSDKError  {
+    public enum WootingAnalogResult  {
         Ok = 1,
         UnInitialized = -2000,
         NoDevices,
@@ -54,64 +54,64 @@ namespace WootingAnalogSDK.NET {
     public delegate void DeviceEventCb(DeviceEventType eventType, IntPtr deviceInfo);
     
     public static class WootingAnalogSDK {
-        public const string SdkLib = "libanalog_sdk_wrapper";
+        public const string SdkLib = "wooting_analog_wrapper";
 
-        [DllImport(SdkLib, EntryPoint = "wasdk_intialise")]
-        public static extern AnalogSDKError Initialise();
+        [DllImport(SdkLib, EntryPoint = "wooting_analog_intialise")]
+        public static extern WootingAnalogResult Initialise();
 
         [DllImport(SdkLib)]
-        private static extern bool wasdk_is_initialised();
+        private static extern bool wooting_analog_is_initialised();
         
         public static bool IsInitialised
         {
-            get => wasdk_is_initialised();
+            get => wooting_analog_is_initialised();
         }
 
-        [DllImport(SdkLib, EntryPoint = "wasdk_uninitialise")]
-        public static extern AnalogSDKError UnInitialise();
+        [DllImport(SdkLib, EntryPoint = "wooting_analog_uninitialise")]
+        public static extern WootingAnalogResult UnInitialise();
 
-        [DllImport(SdkLib, EntryPoint = "wasdk_set_keycode_mode")]
-        public static extern AnalogSDKError SetKeycodeMode(KeycodeType mode);
+        [DllImport(SdkLib, EntryPoint = "wooting_analog_set_keycode_mode")]
+        public static extern WootingAnalogResult SetKeycodeMode(KeycodeType mode);
         
         [DllImport(SdkLib)]
-        private static extern float wasdk_read_analog(ushort code);
+        private static extern float wooting_analog_read_analog(ushort code);
         
         [DllImport(SdkLib)]
-        private static extern float wasdk_read_analog_device(ushort code, ulong deviceId);
+        private static extern float wooting_analog_read_analog_device(ushort code, ulong deviceId);
 
-        public static (float, AnalogSDKError) ReadAnalog(ushort code, ulong deviceId = 0)
+        public static (float, WootingAnalogResult) ReadAnalog(ushort code, ulong deviceId = 0)
         {
-            float res = wasdk_read_analog_device(code, deviceId);
+            float res = wooting_analog_read_analog_device(code, deviceId);
             if (res >= 0)
-                return (res, AnalogSDKError.Ok);
+                return (res, WootingAnalogResult.Ok);
             else
-                return (-1.0f, (AnalogSDKError) (int) res);
+                return (-1.0f, (WootingAnalogResult) (int) res);
         }
 
         
-        [DllImport(SdkLib, EntryPoint = "wasdk_set_device_event_cb")]
-        public static extern AnalogSDKError SetDeviceEventCallback(DeviceEventCb cb);
+        [DllImport(SdkLib, EntryPoint = "wooting_analog_set_device_event_cb")]
+        public static extern WootingAnalogResult SetDeviceEventCallback(DeviceEventCb cb);
 
-        [DllImport(SdkLib, EntryPoint = "wasdk_clear_device_event_cb")]
-        public static extern AnalogSDKError wasdk_clear_device_event_cb();
+        [DllImport(SdkLib, EntryPoint = "wooting_analog_clear_device_event_cb")]
+        public static extern WootingAnalogResult wooting_analog_clear_device_event_cb();
 
-        //fn wasdk_device_info(buffer: *mut Void, len: c_uint) -> c_int;
-        //fn wasdk_read_full_buffer(code_buffer: *mut c_ushort, analog_buffer: *mut c_float, len: c_uint) -> c_int;
+        //fn wooting_analog_device_info(buffer: *mut Void, len: c_uint) -> c_int;
+        //fn wooting_analog_read_full_buffer(code_buffer: *mut c_ushort, analog_buffer: *mut c_float, len: c_uint) -> c_int;
 
         [DllImport(SdkLib)]
-        private static extern int wasdk_read_full_buffer([In][Out][MarshalAs(UnmanagedType.LPArray)] short[] codeBuffer, [In][Out][MarshalAs(UnmanagedType.LPArray)] float[] analogBuffer, uint len);
+        private static extern int wooting_analog_read_full_buffer([In][Out][MarshalAs(UnmanagedType.LPArray)] short[] codeBuffer, [In][Out][MarshalAs(UnmanagedType.LPArray)] float[] analogBuffer, uint len);
         
         [DllImport(SdkLib)]
-        private static extern int wasdk_read_full_buffer_device([In][Out][MarshalAs(UnmanagedType.LPArray)] short[] codeBuffer, [In][Out][MarshalAs(UnmanagedType.LPArray)] float[] analogBuffer, uint len, ulong deviceID);
+        private static extern int wooting_analog_read_full_buffer_device([In][Out][MarshalAs(UnmanagedType.LPArray)] short[] codeBuffer, [In][Out][MarshalAs(UnmanagedType.LPArray)] float[] analogBuffer, uint len, ulong deviceID);
 
-        public static (List<(short, float)>, AnalogSDKError) ReadFullBuffer(uint length, ulong deviceID = 0)
+        public static (List<(short, float)>, WootingAnalogResult) ReadFullBuffer(uint length, ulong deviceID = 0)
         {
             short[] codeBuffer = new short[length];
             float[] analogBuffer = new float[length];
-            int count = wasdk_read_full_buffer_device(codeBuffer, analogBuffer, length, deviceID);
+            int count = wooting_analog_read_full_buffer_device(codeBuffer, analogBuffer, length, deviceID);
 
             if (count < 0)
-                return (null, (AnalogSDKError)count);
+                return (null, (WootingAnalogResult)count);
 
             List<(short, float)> data = new List<(short, float)>();
             for (int i = 0; i < count; i++)
@@ -119,15 +119,15 @@ namespace WootingAnalogSDK.NET {
                 data.Add( (codeBuffer[i], analogBuffer[i]) );
             }
             data.Sort((u, v) => u.Item1.CompareTo(v.Item1));
-            return (data, AnalogSDKError.Ok);
+            return (data, WootingAnalogResult.Ok);
         }
 
         [DllImport(SdkLib)]
-        private static extern int wasdk_get_connected_devices_info([In][Out][MarshalAs(UnmanagedType.LPArray)] IntPtr[] buffer, uint len);
+        private static extern int wooting_analog_get_connected_devices_info([In][Out][MarshalAs(UnmanagedType.LPArray)] IntPtr[] buffer, uint len);
         
-        public static (List<DeviceInfo>, AnalogSDKError) GetConnectedDevicesInfo(){
+        public static (List<DeviceInfo>, WootingAnalogResult) GetConnectedDevicesInfo(){
             IntPtr[] buffer = new IntPtr[40];
-            int count = wasdk_get_connected_devices_info(buffer, (uint)buffer.Length);
+            int count = wooting_analog_get_connected_devices_info(buffer, (uint)buffer.Length);
             if (count > 0)
             {
                 return (buffer.Select<IntPtr, DeviceInfo?>((ptr) =>
@@ -139,10 +139,10 @@ namespace WootingAnalogSDK.NET {
                                    typeof(DeviceInfo));
                     }
                     return null;
-                }).Where(s => s != null).Cast<DeviceInfo>().ToList(), AnalogSDKError.Ok);
+                }).Where(s => s != null).Cast<DeviceInfo>().ToList(), WootingAnalogResult.Ok);
             }
             else
-                return (new List<DeviceInfo>(), (AnalogSDKError)count);
+                return (new List<DeviceInfo>(), (WootingAnalogResult)count);
         }
     }
 }
